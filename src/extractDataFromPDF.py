ffd82789658ff0085b2df1cbe2d5f05885600683
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-26 19:37:06 -0500

    Metada de PDF's
    Version 1.0.0

--- src/extractDataFromPDF.py
+++ src/extractDataFromPDF.py
@@ -1,4 +1,10 @@
-# -*- encoding: utf-8 -*-
+"""
+Uso: Metadata de PDF's
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 26 Abril 2020
+"""
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import os 
